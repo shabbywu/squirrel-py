eaad7ece6b129a435b02adb245d45139bb4b21e2
shabbywu <shabbywu@tencent.com>
2024-04-03 12:36:16 +0800

    minor: 增加 export 对象

--- squirrel-lang/squirrel/__init__.py
+++ squirrel-lang/squirrel/__init__.py
@@ -1,6 +1,14 @@
-from _squirrel import __version__, __author__, SQUIRREL_VERSION
+from _squirrel import __version__, __author__, get_static_vm, StaticVM, SQUIRREL_VERSION
 from _squirrel import SQVM, compile
 from _squirrel.types import *  # noqa: F403
 
 
-__all__ = ["__version__", "__author__", "SQVM", "compile", "SQUIRREL_VERSION"]
+__all__ = [
+    "__version__",
+    "__author__",
+    "get_static_vm",
+    "StaticVM",
+    "SQVM",
+    "compile",
+    "SQUIRREL_VERSION",
+]
