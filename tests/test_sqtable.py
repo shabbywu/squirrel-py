cd04e897ee86aa3c9103c442fd842cf4b63d0abb
shabbywu <shabbywu@qq.com>
2024-06-22 00:13:37 +0800

    test sqtable binding

--- tests/test_sqtable.py
+++ tests/test_sqtable.py
@@ -27,3 +27,33 @@ def test_crud():
     assert sq.test_get() == 2
     sq["flag"] = "test"
     assert str(sq.test_get()) == "test"
+
+
+def test_get():
+    vm = SQVM()
+    vm.execute(
+        """
+        local gt = getroottable();
+        who <- "world"
+        say <- function  () {
+            return "hello " + who;
+        }
+        a <- {
+               who = "a"
+               say = say
+        }
+        b <- {
+               who = "b"
+               say = say
+        }
+    """
+    )
+
+    rt = vm.get_roottable()
+    glboal_say = rt.say
+    a_say = rt.a.say
+    b_say = rt.b.say
+
+    assert a_say() == "hello a"
+    assert b_say() == "hello b"
+    assert glboal_say() == "hello world"
