d78716a7b810e7915accc5ec5d168bbc2cca9040
shabbywu <shabbywu@qq.com>
2024-08-25 21:44:32 +0800

    fix: 修复 getter 无法直接获取 table 的问题

--- squirrel-lang/sqbinding/sqbinding/detail/common/template_getter.hpp
+++ squirrel-lang/sqbinding/sqbinding/detail/common/template_getter.hpp
@@ -3,22 +3,22 @@
 #define SQOBJECTPTR_GETTER_TEMPLATE\
     template <typename TK, typename TV>\
     TV get(TK& key) {\
-        TV r;\
-        if(get(key, r)) {\
-            return r;\
-        }\
+        SQObjectPtr r;\
         VM& vm = holder->GetVM();\
+        if(get(key, r)) {\
+            return GenericCast<TV(SQObjectPtr&)>::cast(vm, r);\
+        }\
         auto sqkey = GenericCast<SQObjectPtr(TK&)>::cast(vm, key);\
         throw sqbinding::key_error(sqobject_to_string(sqkey));\
     }\
 \
     template <typename TK, typename TV>\
     TV get(TK&& key) {\
-        TV r;\
-        if(get(key, r)) {\
-            return r;\
-        }\
+        SQObjectPtr r;\
         VM& vm = holder->GetVM();\
+        if(get(key, r)) {\
+            return GenericCast<TV(SQObjectPtr&)>::cast(vm, r);\
+        }\
         auto sqkey = GenericCast<SQObjectPtr(TK&)>::cast(vm, key);\
         throw sqbinding::key_error(sqobject_to_string(sqkey));\
     }\
