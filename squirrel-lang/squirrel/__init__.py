9aae32d2013214a5b119a83c52f338ab7ef19a41
Nathan C. <149914029+Natuworkguy@users.noreply.github.com>
2026-07-03 02:36:55 +0000

    Fix flake8 E501 in squirrel/__init__.py

--- squirrel-lang/squirrel/__init__.py
+++ squirrel-lang/squirrel/__init__.py
@@ -1,5 +1,13 @@
-from _squirrel import __version__, __author__, get_static_vm, StaticVM, SQUIRREL_VERSION
-from _squirrel import SQVM, compile, compile_bb
+from _squirrel import (
+    __version__,
+    __author__,
+    get_static_vm,
+    StaticVM,
+    SQUIRREL_VERSION,
+    SQVM,
+    compile,
+    compile_bb
+)
 from _squirrel.types import *  # noqa: F401, F403
 
 
